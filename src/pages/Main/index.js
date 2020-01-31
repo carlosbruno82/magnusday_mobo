e748d24d5921984dd7facf698d048dee4dcb5d6b
Carlos Bruno <carlao.linux@gmail.com>
2020-01-30 22:48:17 -0300

    Montando layout

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+import clock from '../../assets/clock.png'
 
 export default function Main() {
   return (
     <View style={styles.container}>
-      <Text>Magnus Day</Text>
+      <Image source={clock}  style={styles.clock}/>
+      <Text style={styles.texto}>00:00:00</Text>
     </View>
   )
 }
@@ -13,6 +16,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems:'center',
+    
+  },
+
+  clock: {
+    width: 370,
+    height: 370,
+    resizeMode: 'contain'
+  },
+
+  texto: {
+    fontSize: 60,
+    color: "#fff",
+    fontWeight: "bold",
+    position: "absolute"
+
   },
 })
