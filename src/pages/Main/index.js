1c963a5297ca442299506e878417a7a661efbb1e
Carlos Bruno <carlao.linux@gmail.com>
2020-02-04 16:05:38 -0300

    audio

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,60 +1,216 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native'
+import { Audio } from 'expo-av'
 
 import clock from '../../assets/clock.png'
 
-import Sound from 'react-native-sound'
+class Cronometro extends Component{
+    
+  constructor(props){
+      super(props)
+      this.state = {
+          numero: '00:00:00',
+          h2: 0,
+          h3: 0,
+          botao: 'PLAY',
+          seconds: 0,
+          minutes: 0,
+          hours: 0,
+          min: 0,
+          minInter: 0,
+          total: 0,
+          totalInter: 0
+      }
+      this.sound = new Audio.Sound()
+      this.beep = this.sound.loadAsync(require('../../assets/beep.mp3'))
+      this.timer = null
+      
+      this.startCounting = this.startCounting.bind(this)
+      this.play = this.play.bind(this)
+      this.limpar = this.limpar.bind(this)
+      this.pauseAudio = this.pauseAudio.bind(this)
+      this.menos = this.menos.bind(this)
+      this.mais = this.mais.bind(this)
+      this.menosInter = this.menosInter.bind(this)
+      this.maisInter = this.maisInter.bind(this)
+      this.beepPlay = this.beepPlay.bind(this)
+  }
 
-export default function Main() {
-  let numero = '00:00:00'
-  let zeroInt = '0'
-  let zeroMin = '0'
-  let botao = 'PLAY'
-  let seconds = 0
-  let minutes = 0
-  let hours = 0
-  let min = 0
-  let minTotal = 0
-  let int = 0
-  let intTotal = 0
+  startCounting = () => {
+      const state = this.state
+      state.seconds++;
+      state.total--;
+      state.totalInter--;
+            if (state.seconds >= 60) {
+                state.seconds = 0;
+                state.minutes++;
+                if (state.minutes >= 60) {
+                    state.minutes = 0;
+                    state.hours++;
+                }
+            }
 
-  let timer = null
-  const beep = new Sound('../../assets/beep.mp3')
-  
+            if(state.total == 0){
+                clearInterval(this.timer)
+                this.timer = null
+                state.botao = 'PLAY'
+                state.min = 0
+                state.minInter = 0
+                state.h2 = state.min
+                state.h3 = state.minInter
+                state.total = 0
+                state.totalInter =0
+                this.beepPlay()
+            } 
 
+            if(state.totalInter == 0){
+              this.beepPlay()
+              state.totalInter = state.minInter * 60
+            } else if(state.totalInter == 57){
+              this.pauseAudio()
+            }
 
+            state.numero = `${state.hours ? (state.hours > 9 ? state.hours : "0" + state.hours) : "00"}:${state.minutes ? (state.minutes > 9 ? state.minutes : "0" + state.minutes) : "00"}:${state.seconds > 9 ? state.seconds : "0" + state.seconds}`
+            this.setState(state)
+  } 
+
+  play = () => {
+      const state = this.state
+      
+      if(this.timer !== null) {
+          clearInterval(this.timer)
+          this.timer = null
+          state.botao = 'PLAY'
+      }   else {
+          this.timer = setInterval(this.startCounting, 1000)
+          state.botao = 'PAUSE'
+          this.pauseAudio()
+      }
+      this.setState(state)
+  }   
+
+  limpar = () => {
+      const state = this.state
+      
+      clearInterval(this.timer) 
+      this.timer = null                 
+      state.botao = 'PLAY'
+      state.numero = '00:00:00'
+      state.seconds = 0
+      state.minutes = 0
+      state.hours = 0
+      state.min = 0
+      state.total = 0
+      state.h2 = state.min
+      state.minInter = 0
+      state.totalInter = 0
+      state.h3 = state.minInter
+      this.pauseAudio()
+      this.setState(state)
+  }
+
+  menos = () => {
+      const state = this.state
+      if(state.min < 0){
+          state.min = 0
+          state.h2 = state.min
+      } else if(state.min > 0){
+          state.min--
+          state.total -= 60
+          state.h2 = state.min
+      }
+      this.setState(state)
+  }
+
+  mais = () => {
+      const state = this.state
+      state.min++
+      state.total += 60
+      state.h2 = state.min
+      this.setState(state)
+  }
+
+  menosInter = () => {
+      const state = this.state
+      if(state.minInter < 0){
+          state.minInter = 0
+          state.h3 = state.minInter
+      } else if(state.minInter > 0){
+          state.minInter--
+          state.totalInter -= 60
+          state.h3 = state.minInter
+      }
+      this.setState(state)
+  }
+
+  maisInter = () => {
+      const state = this.state
+      state.minInter++
+      state.totalInter += 60
+      state.h3 = state.minInter
+      this.setState(state)
+  }
+
+  pauseAudio = () => {
+    try {
+      this.beep.pauseAsync()
+      this.beep.currentTime = 0.0
+    } catch (error) {
+      console.log(error)
+    }
+  }
   
-  
-  
-  return (
+  beepPlay = () => {
+    try {
+      this.beep.replayAsync()
+      this.beep.playAsync()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  render(){
+
+    return (
       <ImageBackground 
         source={clock}
         imageStyle={{resizeMode: 'center'}}
         style={styles.clock}
       >
         <View style={styles.contMin}>
-          <Text style={styles.menos}>-</Text>
-          <Text style={styles.zero}>{zeroMin}</Text>
-          <Text style={styles.mais}>+</Text>
+          <TouchableOpacity onPress={this.menos}>
+            <Text style={styles.menos}>-</Text>
+          </TouchableOpacity>
+          <Text style={styles.zero}>{this.state.h2}</Text>
+          <TouchableOpacity onPress={this.mais}>
+            <Text style={styles.mais}>+</Text>
+          </TouchableOpacity>
         </View>
-
         <View style={styles.container}>
-          <Text style={styles.cronometro}>{numero}</Text>
+          <Text style={styles.cronometro}>{this.state.numero}</Text>
         </View>
-
         <View style={styles.contInt}>
-          <Text style={styles.menos}>-</Text>
-          <Text style={styles.zero}>{zeroInt}</Text>
-          <Text style={styles.mais}>+</Text>
+        <TouchableOpacity onPress={this.menosInter}>
+            <Text style={styles.menos}>-</Text>
+          </TouchableOpacity>
+          <Text style={styles.zero}>{this.state.h3}</Text>
+          <TouchableOpacity onPress={this.maisInter}>
+            <Text style={styles.mais}>+</Text>
+          </TouchableOpacity>
         </View>
-
-        <TouchableOpacity style={styles.botao}>
-          <Text style={styles.botaText}>{botao}</Text>
-          <Text style={styles.botaText}>LIMPAR</Text>
-        </TouchableOpacity>
+        <View style={styles.botao}>
+          <TouchableOpacity  onPress={this.play}>
+            < Text style={styles.botaText}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.limpar}>
+            <Text style={styles.botaText}>LIMPAR</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
-  )
+    )
+  }
 }
+
+export default Cronometro;
 
 const styles = StyleSheet.create({
 
